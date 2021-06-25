40fc0f33a61cc29ee8ef87335224d6c9eee18ea4
NicoVIII <nico@nicoviii.net>
2021-06-25 08:41:49 +0000

    Implement packing in new run script

--- run.fsx
+++ run.fsx
@@ -1,13 +1,17 @@
 #!/bin/dotnet fsi
 
 #r "nuget: Fake.DotNet.Cli"
+#r "nuget: Fake.IO.FileSystem"
 
 open System.IO
 
 open Fake.DotNet
+open Fake.IO
 
 module Config =
+    let srcFolder = "src"
     let testFolder = "tests"
+    let packFolder = "deploy"
 
 // kleisli composition operator for chaining
 let (>=>) fun1 fun2 = fun1 >> (Result.bind fun2)
@@ -22,11 +26,15 @@ module Result =
 
 // I want to use the Result type for results of exec
 let dotnet command args () =
+    printfn $"> dotnet {command} {args}"
+
     DotNet.exec id command args
     |> (fun result -> result.OK)
     |> function
         | true -> Ok()
         | false -> Error()
+
+let isFsproj (file: string) = file.EndsWith ".fsproj"
 
 module Commands =
     let restore =
@@ -38,24 +46,54 @@ module Commands =
         Directory.EnumerateDirectories Config.testFolder
         |> Seq.map Directory.EnumerateFiles
         |> Seq.concat
-        |> Seq.filter (fun file -> file.EndsWith ".fsproj")
+        |> Seq.filter isFsproj
         |> Seq.map
             (fun project ->
                 printfn "\nRun tests in %s:" project
 
-                dotnet "run" $"--project {project}" ())
+                dotnet "run" $"--project \"{project}\"" ())
+        |> Seq.reduce Result.collapse
+
+    /// Packs everything for nuget with given version
+    let pack version () =
+        let outDir = Config.packFolder
+
+        // Clean up output folder
+        Directory.delete outDir
+
+        // We pack every project in the src folder
+        Directory.EnumerateDirectories Config.srcFolder
+        |> Seq.map Directory.EnumerateFiles
+        |> Seq.concat
+        |> Seq.filter isFsproj
+        |> Seq.map (fun project -> dotnet "pack" $"-c Release -o \"{outDir}\" /p:Version=%s{version} \"{project}\"" ())
         |> Seq.reduce Result.collapse
 
 // We determine, what we want to execute
-let execute =
+let execute args =
     // We read the command from arguments
-    Array.tryItem 1 fsi.CommandLineArgs
-    |> Option.defaultValue ""
+    let command =
+        Array.tryItem 1 args |> Option.defaultValue ""
+
+    // Read arguments for commands
+    let commandArgs =
+        if Array.length args > 2 then
+            Array.skip 2 args |> Array.toList
+        else
+            []
+
+    (command, commandArgs)
     // We determine which commands to run
     |> function
-        | "restore" -> Commands.restore
-        | "build" -> Commands.build
-        | "test" -> Commands.build >=> Commands.test
+        | "restore", [] -> Commands.restore
+        | "build", [] -> Commands.build
+        | "test", [] -> Commands.build >=> Commands.test
+        | "pack", [ version ] -> Commands.build >=> Commands.pack version
+        // Catch errors and help
+        | "pack", [] ->
+            (fun _ ->
+                printfn "pack command needs one argument, which is the version to pack for."
+                Error())
         | _ ->
             (fun _ ->
                 printfn "Usage: (./run.fsx | dotnet fsi run.fsx) <command>"
@@ -63,7 +101,7 @@ let execute =
                 Error())
 
 // We execute it and map the result to an exit code
-execute ()
+execute fsi.CommandLineArgs ()
 |> function
     | Ok () -> 0
     | Error () -> 1
